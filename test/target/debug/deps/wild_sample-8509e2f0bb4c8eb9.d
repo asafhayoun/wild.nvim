8a8c85c3fd164e45691bfe1010e8ef04ca53e56c
Asaf Hayoun <asafhay2009@gmail.com>
2023-06-30 14:12:06 +0300

    Removed unnecessary files

--- test/target/debug/deps/wild_sample-8509e2f0bb4c8eb9.d
+++
@@ -1,5 +0,0 @@
-/Users/asafhay/Codes/themes/wild.nvim/test/target/debug/deps/wild_sample-8509e2f0bb4c8eb9.rmeta: src/main.rs
-
-/Users/asafhay/Codes/themes/wild.nvim/test/target/debug/deps/wild_sample-8509e2f0bb4c8eb9.d: src/main.rs
-
-src/main.rs:
--- test/target/debug/deps/wild_sample-a202f5dd25f11b32.d
+++
@@ -1,5 +0,0 @@
-/Users/asafhay/Codes/themes/wild.nvim/test/target/debug/deps/wild_sample-a202f5dd25f11b32.rmeta: src/main.rs
-
-/Users/asafhay/Codes/themes/wild.nvim/test/target/debug/deps/wild_sample-a202f5dd25f11b32.d: src/main.rs
-
-src/main.rs:
